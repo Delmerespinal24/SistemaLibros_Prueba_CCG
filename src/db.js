8676ed0ad206e95085bdab34e34ae77b6a40ebc7
Delmerespinal24 <Delmerespinal2@gmail.com>
2024-10-21 17:23:04 -0600

    Update db.js

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: 'localhost', 
   user: 'root', 
-  password: 'belinda98', 
+  password: 'password123', 
   database: 'BOOKSTORE_CCG' 
 });
 
